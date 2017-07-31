0661b0f89df3df7ed9f9dc6ded800935e5fe7d85
Andronik Ordian <write@reusable.software>
2017-07-31 17:22:15 +0300

    fix(codecov): use custom shell script

---
+++ .travis/coverage.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+function install_kcov_from_master() {
+    wget https://github.com/SimonKagstrom/kcov/archive/master.zip &&
+        unzip master.zip &&
+        mkdir kcov-master/build &&
+        pushd kcov-master/build &&
+        cmake .. &&
+        make &&
+        make install DESTDIR=../../kcov &&
+        popd &&
+        rm -rf kcov-master master.zip
+}
+
+function build_coverage() {
+    local kcov=./kcov/usr/local/bin/kcov
+
+    for file in target/debug/{toml_edit-,test_}*; do
+        if [[ "${file: -2}" != ".d" ]]; then
+            local bin="target/kcov-$(basename $file)";
+            mkdir "$bin";
+            $kcov --exclude-pattern=/.cargo,/usr/lib --verify "$bin" "$file";
+        fi;
+    done
+
+    $kcov --merge target/kcov target/kcov-*
+}
+
+function report_coverage_to_codecov() {
+    bash <(curl -s https://codecov.io/bash) -s target/kcov
+}
+
+install_kcov_from_master
+build_coverage
+report_coverage_to_codecov
