701b05298ea44cb6f7c3b78a8509e139fe9a48bb
Andronik Ordian <write@reusable.software>
2017-08-04 21:00:35 +0300

    fix(fuzz): update fuzzer to new parse API

--- fuzz/parse_document.rs
+++ fuzz/parse_document.rs
@@ -3,11 +3,11 @@
 #[macro_use] extern crate libfuzzer_sys;
 extern crate toml_edit;
 
-use toml_edit::{Document};
+use toml_edit::Document;
 
 fuzz_target!(|data| {
     if let Ok(data) = std::str::from_utf8(data) {
-        let doc = Document::parse(data);
+        let doc = data.parse::<Document>();
         assert!(doc.is_ok());
         let doc = doc.unwrap();
         assert_eq!(doc.to_string(), data);
