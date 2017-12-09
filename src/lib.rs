67d3d004defc11e0cc1e83c8631aaf783c6e8159
Andronik Ordian <write@reusable.software>
2017-12-09 18:44:52 +0300

    doc(readme): add more comments

--- src/lib.rs
+++ src/lib.rs
@@ -23,9 +23,9 @@
 //!     "#;
 //!     let mut doc = toml.parse::<Document>().expect("invalid doc");
 //!     assert_eq!(doc.to_string(), toml);
-//!     // let's add a new key/value pair
+//!     // let's add a new key/value pair inside a.b: c = {d = "hello"}
 //!     doc["a"]["b"]["c"]["d"] = value("hello");
-//!     // autoformat inline table a.b.c
+//!     // autoformat inline table a.b.c: { d = "hello" }
 //!     doc["a"]["b"]["c"].as_inline_table_mut().map(|t| t.fmt());
 //!     let expected = r#"
 //! "hello" = 'toml!' # comment
