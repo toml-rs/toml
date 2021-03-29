86652087a7fea4052f074e919379932a2ac00ac5
Riey <creeper844@gmail.com>
2021-03-30 05:40:21 +0900

    Add IterMut for Table (#100)
    * Add Table::IterMut
    
    * Export Table::IterMut

--- src/lib.rs
+++ src/lib.rs
@@ -84,6 +84,6 @@ pub use crate::decor::Decor;
 pub use crate::document::Document;
 pub use crate::key::Key;
 pub use crate::parser::TomlError;
-pub use crate::table::{array, table, value, Item, Iter, Table, TableLike};
+pub use crate::table::{array, table, value, Item, Iter, IterMut, Table, TableLike};
 pub use crate::value::{Array, ArrayIter, InlineTable, Value};
 pub use formatted::decorated;
