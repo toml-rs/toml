318d240e52b7cb9785257567807fbb70809795a3
Ed Page <eopage@gmail.com>
2021-09-07 16:11:41 -0500

    Baseline

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -44,7 +44,7 @@
 //! The easiest way to parse a TOML document is via the [`Value`] type:
 //!
 //! ```rust
-//! use toml_edit::easy::Value;
+//! use toml::Value;
 //!
 //! let value = "foo = 'bar'".parse::<Value>().unwrap();
 //!
@@ -93,7 +93,7 @@
 //! }
 //!
 //! fn main() {
-//!     let config: Config = toml_edit::easy::from_str(r#"
+//!     let config: Config = toml::from_str(r#"
 //!         ip = '127.0.0.1'
 //!
 //!         [keys]
@@ -136,7 +136,7 @@
 //!         },
 //!     };
 //!
-//!     let toml = toml_edit::easy::to_string(&config).unwrap();
+//!     let toml = toml::to_string(&config).unwrap();
 //! }
 //! ```
 //!
@@ -144,6 +144,37 @@
 //! [Cargo]: https://crates.io/
 //! [`serde`]: https://serde.rs/
 
-pub mod de;
+#![doc(html_root_url = "https://docs.rs/toml/0.5")]
+#![deny(missing_docs)]
+#![warn(rust_2018_idioms)]
+// Makes rustc abort compilation if there are any unsafe blocks in the crate.
+// Presence of this annotation is picked up by tools such as cargo-geiger
+// and lets them ensure that there is indeed no unsafe code as opposed to
+// something they couldn't detect (e.g. unsafe added via macro expansion, etc).
+#![forbid(unsafe_code)]
 
+pub mod map;
+pub mod value;
+#[doc(no_inline)]
+pub use crate::value::Value;
+mod datetime;
+
+pub mod ser;
+#[doc(no_inline)]
+pub use crate::ser::{to_string, to_string_pretty, to_vec, Serializer};
+pub mod de;
+#[doc(no_inline)]
 pub use de::{from_document, from_slice, from_str, Deserializer};
+mod tokens;
+
+#[doc(hidden)]
+pub mod macros;
+
+mod spanned;
+pub use crate::spanned::Spanned;
+
+// Just for rustdoc
+#[allow(unused_imports)]
+use crate::datetime::Datetime;
+#[allow(unused_imports)]
+use core::str::FromStr;
