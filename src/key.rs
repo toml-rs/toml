dc7bc2a7a375394bc4b22dc7a4e84d75db826b3a
Ed Page <eopage@gmail.com>
2021-12-14 13:10:46 -0600

    fix!: Make `Key::from_str` only accept `Display`s output
    I remember wanting to remove this before and had problems.  It looks
    like those have since been addressed (I think with changes to repr).
    
    Before, we were taking any string and trying to infer what its repr
    would be.
    
    Now we only parse actualy dotted keys.
    
    This will hopefully help with rust-lang/cargo#10176
    
    BREAKING CHANGE: `Key::from_str` will accept fewer values and will
    return slightly different errors.

--- src/key.rs
+++ src/key.rs
@@ -158,11 +158,7 @@ impl FromStr for Key {
     /// if fails, tries as basic quoted key (surrounds with "")
     /// and then literal quoted key (surrounds with '')
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let basic = format!("\"{}\"", s);
-        let literal = format!("'{}'", s);
         Key::try_parse(s)
-            .or_else(|_| Key::try_parse(&basic))
-            .or_else(|_| Key::try_parse(&literal))
     }
 }
 
