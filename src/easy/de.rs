028e72d9d09ca7dd7ac0c29577fb499a0c787447
Ed Page <eopage@gmail.com>
2022-01-26 15:35:15 -0600

    chore: Remove dead code

--- src/easy/de.rs
+++
@@ -1,5 +0,0 @@
-pub use crate::de::Deserializer;
-pub use crate::de::Error;
-pub use crate::de::from_slice;
-pub use crate::de::from_document;
-pub use crate::de::from_str;
--- src/easy/ser.rs
+++
@@ -1,6 +0,0 @@
-pub use crate::ser::Serializer;
-pub use crate::ser::Error;
-pub use crate::ser::to_document;
-pub use crate::ser::to_string;
-pub use crate::ser::to_string_pretty;
-pub use crate::ser::to_vec;
