4c776a43f110d0ac64060b48046d7236c2ff98e8
Ed Page <eopage@gmail.com>
2023-01-02 22:47:30 -0600

    fix(parser): Catch more dotted key interactions

--- crates/toml_edit/tests/decoder_compliance.rs
+++ crates/toml_edit/tests/decoder_compliance.rs
@@ -3,12 +3,6 @@ mod decoder;
 fn main() {
     let decoder = decoder::Decoder;
     let mut harness = toml_test_harness::DecoderHarness::new(decoder);
-    harness
-        .ignore([
-            "valid/string/escape-esc.toml",
-            "invalid/table/duplicate-key-dotted-table.toml",
-            "invalid/table/duplicate-key-dotted-table2.toml",
-        ])
-        .unwrap();
+    harness.ignore(["valid/string/escape-esc.toml"]).unwrap();
     harness.test();
 }
