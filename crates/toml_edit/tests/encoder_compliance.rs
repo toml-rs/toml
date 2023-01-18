1ad6eb0fe038286f2628e565be229308531ba096
Ed Page <eopage@gmail.com>
2023-01-18 08:45:50 -0600

    test: Update compliance cases

--- crates/toml_edit/tests/encoder_compliance.rs
+++ crates/toml_edit/tests/encoder_compliance.rs
@@ -4,6 +4,7 @@ mod encoder;
 fn main() {
     let encoder = encoder::Encoder;
     let decoder = decoder::Decoder;
-    let harness = toml_test_harness::EncoderHarness::new(encoder, decoder);
+    let mut harness = toml_test_harness::EncoderHarness::new(encoder, decoder);
+    harness.ignore(["valid/spec/float-0.toml"]).unwrap();
     harness.test();
 }
