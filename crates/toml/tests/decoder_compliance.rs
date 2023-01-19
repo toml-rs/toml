4bf527dfd8f913a7a43ec52c0dad2223a9c31682
Ed Page <eopage@gmail.com>
2023-01-19 16:24:01 -0600

    test(toml): Update ignored compliance tests
    This is another example of the benefit of #340

--- crates/toml/tests/decoder_compliance.rs
+++ crates/toml/tests/decoder_compliance.rs
@@ -5,18 +5,12 @@ fn main() {
     let mut harness = toml_test_harness::DecoderHarness::new(decoder);
     harness
         .ignore([
-            "invalid/control/comment-cr.toml",
-            "invalid/control/comment-del.toml",
-            "invalid/datetime/hour-over.toml",
-            "invalid/integer/positive-bin.toml",
-            "invalid/integer/positive-hex.toml",
-            "valid/inline-table/newline.toml",
             "valid/spec/float-0.toml",
-            "valid/spec/table-9.toml",
             // Unreleased
             "valid/string/escape-esc.toml",
             "valid/string/hex-escape.toml",
             "valid/datetime/no-seconds.toml",
+            "valid/inline-table/newline.toml",
         ])
         .unwrap();
     harness.test();
