252da49940768c7b7acde7e6585024d85559f5c8
Ed Page <eopage@gmail.com>
2023-01-19 16:43:49 -0600

    test: Shift serde tests to 'toml'
    Depending on the case, a serde tests could live in either or both
    crates, so let's simplify by just saying all serde tests live in `toml`.
    This as the added benefit in that it makes sure the newtype wrappers
    don't break things.

--- crates/toml_edit/tests/testsuite/main.rs
+++ crates/toml_edit/tests/testsuite/main.rs
@@ -2,13 +2,6 @@
 
 mod convert;
 mod edit;
-mod enum_external_deserialize;
-mod formatter;
 mod invalid;
-mod macros;
 mod parse;
-mod pretty;
-mod serde;
-mod spanned;
-mod spanned_impls;
 mod stackoverflow;
