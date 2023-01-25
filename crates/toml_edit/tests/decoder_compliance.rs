ab84da1c586d7e7ed1efe9f893f33bcbad54ea1c
Ed Page <eopage@gmail.com>
2023-01-25 10:30:00 -0600

    test: Document why spec/float-0 fails

--- crates/toml_edit/tests/decoder_compliance.rs
+++ crates/toml_edit/tests/decoder_compliance.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut harness = toml_test_harness::DecoderHarness::new(decoder);
     harness
         .ignore([
-            "valid/spec/float-0.toml",
+            "valid/spec/float-0.toml", // Test issue; `Decoder` turns `6.626e-34` into `0.0`
             // Unreleased
             "valid/string/escape-esc.toml",
             "valid/string/hex-escape.toml",
--- crates/toml_edit/tests/encoder_compliance.rs
+++ crates/toml_edit/tests/encoder_compliance.rs
@@ -5,6 +5,10 @@ fn main() {
     let encoder = encoder::Encoder;
     let decoder = decoder::Decoder;
     let mut harness = toml_test_harness::EncoderHarness::new(encoder, decoder);
-    harness.ignore(["valid/spec/float-0.toml"]).unwrap();
+    harness
+        .ignore([
+            "valid/spec/float-0.toml", // Test issue; `Decoder` turns `6.626e-34` into `0.0`
+        ])
+        .unwrap();
     harness.test();
 }
