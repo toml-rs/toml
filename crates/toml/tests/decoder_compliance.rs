8b66ed7fb6ff00dcc3f710ae494b58bae214a7bc
Ed Page <eopage@gmail.com>
2023-10-03 10:12:22 -0500

    test: Remove false failure

--- crates/toml/tests/decoder_compliance.rs
+++ crates/toml/tests/decoder_compliance.rs
@@ -4,7 +4,6 @@ mod decoder;
 fn main() {
     let decoder = decoder::Decoder;
     let mut harness = toml_test_harness::DecoderHarness::new(decoder);
-    harness.ignore(["valid/spec/float-0.toml"]).unwrap();
     harness.version("1.0.0");
     harness.test();
 }
--- crates/toml/tests/encoder_compliance.rs
+++ crates/toml/tests/encoder_compliance.rs
@@ -6,7 +6,6 @@ fn main() {
     let encoder = encoder::Encoder;
     let decoder = decoder::Decoder;
     let mut harness = toml_test_harness::EncoderHarness::new(encoder, decoder);
-    harness.ignore(["valid/spec/float-0.toml"]).unwrap();
     harness.version("1.0.0");
     harness.test();
 }
