f30de96d85d7cc107e8a21936e5a526345f91b96
Ed Page <eopage@gmail.com>
2024-07-15 15:50:37 -0500

    refactor(fuzz): Simplify code

--- crates/toml_edit_fuzz/parse_document.rs
+++ crates/toml_edit_fuzz/parse_document.rs
@@ -3,25 +3,27 @@
 use toml_edit::DocumentMut;
 
 libfuzzer_sys::fuzz_target!(|data| {
-    if let Ok(data) = std::str::from_utf8(data) {
-        println!("parsing: {data:?}");
-        let doc = match data.parse::<DocumentMut>() {
-            Ok(doc) => doc,
-            Err(err) => {
-                println!("{err}");
-                return;
-            }
-        };
-        let toml = doc.to_string();
-        println!("parsing: {toml:?}");
-        let doc = toml.parse::<DocumentMut>();
-        assert!(
-            doc.is_ok(),
-            "Failed to parse `doc.to_string()`: {}\n```\n{}\n```",
-            doc.unwrap_err(),
-            toml
-        );
-        let doc = doc.unwrap();
-        assert_eq!(doc.to_string(), toml);
-    }
+    let Ok(data) = std::str::from_utf8(data) else {
+        return;
+    };
+
+    println!("parsing: {data:?}");
+    let doc = match data.parse::<DocumentMut>() {
+        Ok(doc) => doc,
+        Err(err) => {
+            println!("{err}");
+            return;
+        }
+    };
+    let toml = doc.to_string();
+    println!("parsing: {toml:?}");
+    let doc = toml.parse::<DocumentMut>();
+    assert!(
+        doc.is_ok(),
+        "Failed to parse `doc.to_string()`: {}\n```\n{}\n```",
+        doc.unwrap_err(),
+        toml
+    );
+    let doc = doc.unwrap();
+    assert_eq!(doc.to_string(), toml);
 });
