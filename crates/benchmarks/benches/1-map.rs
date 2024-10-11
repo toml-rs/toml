5ed33ea3b7072cafd8bbad691cbe329f0636dc86
Ed Page <eopage@gmail.com>
2024-10-11 09:20:42 -0500

    bench: Reorganize benches

---
+++ crates/benchmarks/benches/1-map.rs
@@ -0,0 +1,55 @@
+#![allow(elided_lifetimes_in_paths)]
+
+const NUM_ENTRIES: &[usize] = &[10, 100];
+
+mod toml_edit {
+    use crate::gen;
+    use crate::NUM_ENTRIES;
+
+    #[divan::bench(args = NUM_ENTRIES)]
+    fn document(bencher: divan::Bencher, num_entries: usize) {
+        bencher
+            .with_inputs(|| gen(num_entries))
+            .input_counter(divan::counter::BytesCount::of_str)
+            .bench_values(|sample| sample.parse::<toml_edit::DocumentMut>().unwrap());
+    }
+}
+
+mod toml {
+    use crate::gen;
+    use crate::NUM_ENTRIES;
+
+    #[divan::bench(args = NUM_ENTRIES)]
+    fn document(bencher: divan::Bencher, num_entries: usize) {
+        bencher
+            .with_inputs(|| gen(num_entries))
+            .input_counter(divan::counter::BytesCount::of_str)
+            .bench_values(|sample| sample.parse::<toml::Table>().unwrap());
+    }
+}
+
+mod toml_v05 {
+    use crate::gen;
+    use crate::NUM_ENTRIES;
+
+    #[divan::bench(args = NUM_ENTRIES)]
+    fn document(bencher: divan::Bencher, num_entries: usize) {
+        bencher
+            .with_inputs(|| gen(num_entries))
+            .input_counter(divan::counter::BytesCount::of_str)
+            .bench_values(|sample| sample.parse::<toml_old::Value>().unwrap());
+    }
+}
+
+fn gen(num_entries: usize) -> String {
+    let mut s = String::new();
+    for i in 0..num_entries {
+        s += &format!("[header_no_{i}]\n");
+        s += "entry = 42\n";
+    }
+    s
+}
+
+fn main() {
+    divan::main();
+}
