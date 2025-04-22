79abdcb96cd685b3daf45adec84080548f3e5e0d
Ed Page <eopage@gmail.com>
2025-04-22 14:59:16 -0500

    test: Decouple serde tests

--- crates/toml/tests/testsuite/main.rs
+++ crates/toml/tests/testsuite/main.rs
@@ -1,12 +1,5 @@
 #![recursion_limit = "256"]
 #![cfg(all(feature = "parse", feature = "display"))]
 
-mod de_errors;
 mod display;
-mod enum_external_deserialize;
-mod formatting;
 mod macros;
-mod pretty;
-mod serde;
-mod spanned;
-mod tables_last;
