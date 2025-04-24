74a5745690eebdd3e1c446c158bdb7abbbfbb1b4
Ed Page <eopage@gmail.com>
2025-04-24 14:37:52 -0500

    refactor(edit): Generalize ser for diff with toml

--- crates/toml_edit/src/ser/key.rs
+++ crates/toml_edit/src/ser/key.rs
@@ -7,86 +7,86 @@ pub(crate) struct KeySerializer;
 impl serde::ser::Serializer for KeySerializer {
     type Ok = Key;
     type Error = Error;
-    type SerializeSeq = serde::ser::Impossible<Key, Error>;
-    type SerializeTuple = serde::ser::Impossible<Key, Error>;
-    type SerializeTupleStruct = serde::ser::Impossible<Key, Error>;
-    type SerializeTupleVariant = serde::ser::Impossible<Key, Error>;
-    type SerializeMap = serde::ser::Impossible<Key, Error>;
-    type SerializeStruct = serde::ser::Impossible<Key, Error>;
-    type SerializeStructVariant = serde::ser::Impossible<Key, Error>;
+    type SerializeSeq = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeTuple = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeTupleStruct = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeTupleVariant = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeMap = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeStruct = serde::ser::Impossible<Self::Ok, Error>;
+    type SerializeStructVariant = serde::ser::Impossible<Self::Ok, Error>;
 
-    fn serialize_bool(self, _v: bool) -> Result<Key, Self::Error> {
+    fn serialize_bool(self, _v: bool) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_i8(self, _v: i8) -> Result<Key, Self::Error> {
+    fn serialize_i8(self, _v: i8) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_i16(self, _v: i16) -> Result<Key, Self::Error> {
+    fn serialize_i16(self, _v: i16) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_i32(self, _v: i32) -> Result<Key, Self::Error> {
+    fn serialize_i32(self, _v: i32) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_i64(self, _v: i64) -> Result<Key, Self::Error> {
+    fn serialize_i64(self, _v: i64) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_u8(self, _v: u8) -> Result<Key, Self::Error> {
+    fn serialize_u8(self, _v: u8) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_u16(self, _v: u16) -> Result<Key, Self::Error> {
+    fn serialize_u16(self, _v: u16) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_u32(self, _v: u32) -> Result<Key, Self::Error> {
+    fn serialize_u32(self, _v: u32) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_u64(self, _v: u64) -> Result<Key, Self::Error> {
+    fn serialize_u64(self, _v: u64) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_f32(self, _v: f32) -> Result<Key, Self::Error> {
+    fn serialize_f32(self, _v: f32) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_f64(self, _v: f64) -> Result<Key, Self::Error> {
+    fn serialize_f64(self, _v: f64) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_char(self, _v: char) -> Result<Key, Self::Error> {
+    fn serialize_char(self, _v: char) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_str(self, value: &str) -> Result<Key, Self::Error> {
+    fn serialize_str(self, value: &str) -> Result<Self::Ok, Self::Error> {
         Ok(Key::new(value))
     }
 
-    fn serialize_bytes(self, _value: &[u8]) -> Result<Key, Self::Error> {
+    fn serialize_bytes(self, _value: &[u8]) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_none(self) -> Result<Key, Self::Error> {
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_some<T>(self, _value: &T) -> Result<Key, Self::Error>
+    fn serialize_some<T>(self, _value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: serde::ser::Serialize + ?Sized,
     {
         Err(Error::key_not_string())
     }
 
-    fn serialize_unit(self) -> Result<Key, Self::Error> {
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
-    fn serialize_unit_struct(self, _name: &'static str) -> Result<Key, Self::Error> {
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
         Err(Error::key_not_string())
     }
 
@@ -95,11 +95,11 @@ impl serde::ser::Serializer for KeySerializer {
         _name: &'static str,
         _variant_index: u32,
         variant: &'static str,
-    ) -> Result<Key, Self::Error> {
+    ) -> Result<Self::Ok, Self::Error> {
         Ok(variant.into())
     }
 
-    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<Key, Self::Error>
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: serde::ser::Serialize + ?Sized,
     {
@@ -112,7 +112,7 @@ impl serde::ser::Serializer for KeySerializer {
         _variant_index: u32,
         _variant: &'static str,
         _value: &T,
-    ) -> Result<Key, Self::Error>
+    ) -> Result<Self::Ok, Self::Error>
     where
         T: serde::ser::Serialize + ?Sized,
     {
