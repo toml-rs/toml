c02f74a68bc695d11ffde264faa82b90f76784b9
Ed Page <eopage@gmail.com>
2025-04-24 14:45:01 -0500

    fix(write): Propery format f32 like f64

--- crates/toml_write/src/value.rs
+++ crates/toml_write/src/value.rs
@@ -97,7 +97,19 @@ impl WriteTomlValue for i128 {
 
 impl WriteTomlValue for f32 {
     fn write_toml_value<W: TomlWrite + ?Sized>(&self, writer: &mut W) -> core::fmt::Result {
-        write!(writer, "{self}")
+        match (self.is_sign_negative(), self.is_nan(), *self == 0.0) {
+            (true, true, _) => write!(writer, "-nan"),
+            (false, true, _) => write!(writer, "nan"),
+            (true, false, true) => write!(writer, "-0.0"),
+            (false, false, true) => write!(writer, "0.0"),
+            (_, false, false) => {
+                if self % 1.0 == 0.0 {
+                    write!(writer, "{self}.0")
+                } else {
+                    write!(writer, "{self}")
+                }
+            }
+        }
     }
 }
 
