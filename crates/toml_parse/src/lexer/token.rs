epage/toml_edit

feat(parser): Event emitter skeleton
As every token causes an error, I bulk-allocate for errors to reduce its
overhead skewing the results.

crates/toml_parse/src/lexer/token.rs
@@ -29,6 +29,15 @@ impl<'i> Token<'i> {
     pub fn raw(&self) -> Raw<'i> {
         self.raw
     }
+
+    pub fn to_error(self, expected: &'static [crate::Expected]) -> crate::ParseError<'i> {
+        crate::ParseError {
+            context: self.raw(),
+            description: self.kind().description(),
+            expected,
+            unexpected: self.raw(),
+        }
+    }
 }
 
 impl<'i> std::fmt::Display for Token<'i> {

toml-rs/toml

crates/toml_parse/src/lexer/token.rs
@@ -27,6 +27,15 @@ impl Token {
     pub fn span(&self) -> Span {
         self.span
     }
+
+    pub fn to_error(self, expected: &'static [crate::Expected]) -> crate::ParseError {
+        crate::ParseError {
+            context: self.span(),
+            description: self.kind().description(),
+            expected,
+            unexpected: self.span(),
+        }
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]