17dc3ddbc57573762e8afd2b9d8945dc572615ab
Ed Page <eopage@gmail.com>
2026-02-12 09:54:35 -0600

    fix(parser): Plug another whole in synthetic events

--- crates/toml_edit/tests/compliance/parse.rs
+++ crates/toml_edit/tests/compliance/parse.rs
@@ -658,10 +658,19 @@ extra assignment between key-value pairs, expected `,`
 }
 
 #[test]
-#[should_panic = "setting a value should set a prefix"]
 fn garbage2() {
     let err = "={=<=u==}".parse::<crate::RustDocument>().unwrap_err();
-    assert_data_eq!(err.to_string(), str![]);
+    assert_data_eq!(
+        err.to_string(),
+        str![[r#"
+TOML parse error at line 1, column 5
+  |
+1 | ={=<=u==}
+  |     ^
+extra assignment between key-value pairs, expected `,`
+
+"#]]
+    );
 }
 
 #[test]
