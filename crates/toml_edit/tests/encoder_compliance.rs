170ebca443349fdcae027eed5427f093ff880674
Ed Page <eopage@gmail.com>
2026-02-18 11:37:41 -0600

    test(edit): Ipdate encoder compliance to 1.1

--- crates/toml_edit/tests/encoder_compliance.rs
+++ crates/toml_edit/tests/encoder_compliance.rs
@@ -5,6 +5,6 @@ fn main() {
     let encoder = encoder::Encoder;
     let decoder = decoder::Decoder;
     let mut harness = toml_test_harness::EncoderHarness::new(encoder, decoder);
-    harness.version("1.0.0");
+    harness.version("1.1.0");
     harness.test();
 }
