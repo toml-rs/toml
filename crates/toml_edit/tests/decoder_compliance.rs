8413dbb6d93659fdfce80c90827f6efde61aa586
Ed Page <eopage@gmail.com>
2026-02-18 11:27:41 -0600

    fix(toml): Don't error on dotted keys extending implicit tables
    Fixes #1106

--- crates/toml_edit/tests/decoder_compliance.rs
+++ crates/toml_edit/tests/decoder_compliance.rs
@@ -1,6 +1,29 @@
 mod decoder;
 
 fn main() {
+    let valid_ext = walkdir::WalkDir::new("tests/fixtures/valid")
+        .sort_by_file_name()
+        .into_iter()
+        .map(Result::unwrap)
+        .filter(|e| e.path().extension() == Some(std::ffi::OsStr::new("toml")))
+        .map(|e| {
+            let name = e
+                .path()
+                .strip_prefix("tests/fixtures")
+                .unwrap()
+                .to_owned()
+                .into();
+            let fixture = std::fs::read(e.path()).unwrap().into();
+            let expected_path = e.path().with_extension("json");
+            let expected = std::fs::read(expected_path).unwrap().into();
+            toml_test_data::Valid {
+                name,
+                fixture,
+                expected,
+            }
+        })
+        .collect::<Vec<_>>();
+
     let invalid_ext = walkdir::WalkDir::new("tests/fixtures/invalid")
         .sort_by_file_name()
         .into_iter()
@@ -23,6 +46,7 @@ fn main() {
     harness.version("1.1.0");
     harness.ignore([]).unwrap();
     harness.snapshot_root("tests/snapshots");
+    harness.extend_valid(valid_ext);
     harness.extend_invalid(invalid_ext);
     harness.test();
 }
